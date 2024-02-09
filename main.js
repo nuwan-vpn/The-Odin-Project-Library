0608d28f289d311badf6597fbed4f4973650e229
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-02-09 23:44:40 +0530

    Update render function to display book details in cards and add CSS styles for book cards display
    - Enhanced the render function to display book details in cards format.
    - Each book is now displayed within a div with the card class.
    - Book details such as title, author, pages, and read status are displayed within the card.
    - Utilized ternary operator to dynamically display 'Read' or 'Not yet read' based on the book's read status.
    - Added styles for border, border-radius, padding, margin, background color, and box-shadow to give the cards a clean and visually appealing appearance.
    - Adjusted font sizes, colors, and margins for title, author, and details to improve readability and visual hierarchy.

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
-    // this.notRead  = notRead;
+   
 }
 
 
@@ -15,7 +15,6 @@ function addBookToLibrary() {
     let author = document.querySelector("#author").value;
     let pages = document.querySelector("#pages").value;
     let read = document.querySelector("#read").checked;
-    // let notRead = document.querySelector("#notRead").checked;
     let newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
     render();
@@ -27,7 +26,14 @@ function render(){
     for(let i=0; i< myLibrary.length; i++){
         let book = myLibrary[i];
         let bookEl = document.createElement("div");
-        bookEl.innerHTML = `<p>${book.title}</p>`;
+        bookEl.innerHTML = `
+        <div class="card"> 
+            <h3>${book.title}</h2>
+            <h5>by ${book.author}</h3>
+            <p>pages:${book.pages}</p>
+            <p>${book.read ? "Read":"Not yet read"}</p>
+        </div>
+        `;
         libraryEl.appendChild(bookEl);
     }
 }
