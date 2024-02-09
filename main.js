0bff680cf8555efc3bc03110bdfcb5438b8c4853
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-02-09 11:54:32 +0530

    Implement function to add book to library
    - Created a Book constructor function to initialize book objects with title, author, pages, read, and notRead properties.
    - Implemented the addBookToLibrary function to retrieve input values from the form and store them in variables.

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 
 const myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title, author, pages, read, notRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.notRead  = notRead;
 }
 
+
 function addBookToLibrary() {
-  // do stuff here
+    let title = document.querySelector("#title").value;
+    let author = document.querySelector("#author").value;
+    let pages = document.querySelector("#pages").value;
+    let read = document.querySelector("#read").value;
+    let notRead = document.querySelector("#notRead").value;
 }
 
 document.addEventListener("DOMContentLoaded", function() {
